c02d2f6bd07b9c6710719af67f8ca2eb0a59dd78
Joseph <74473791+sephf@users.noreply.github.com>
2024-09-25 10:40:20 -0600

    config

--- backend/src/main/java/com/proyecto/backend/controller/Controller.java
+++ backend/src/main/java/com/proyecto/backend/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.proyecto.backend;
+package com.proyecto.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
